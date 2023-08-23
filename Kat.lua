66636f5fa764ea9d18d41cc59e12c53faf637fe2
katbozo <142151193+katbozo@users.noreply.github.com>
2023-08-23 08:16:00 +0400

    Create Kat.lua

---
+++ Kat.lua
@@ -0,0 +1,12 @@
+--[[
+
+   |\---/|
+   | ,_, |
+    \_`_/-..----.
+ ___/ `   ' ,""+ \  Meow
+(__...'   __\    |`.___.';
+  (_,...'(_,.`__)/'.....+
+  
+]]--
+
+loadstring(game:HttpGet('https://raw.githubusercontent.com/katbozo/Stuff/main/KatHub.lua'))()
